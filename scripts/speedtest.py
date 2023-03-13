4edadb1e3b5020e4159d78e29eee7f556a0e8048
Daniel Richards <danrichards678@gmail.com>
2023-03-13 11:43:17 +1100

    working script to plot right thing!

--- scripts/speedtest.py
+++ scripts/speedtest.py
@@ -1,5 +1,6 @@
 #%%
 import jax.numpy as jnp
+import numpy as np
 import matplotlib.pyplot as plt
 from meso_fab_mc import meso_mc_jax as mc_jax
 from meso_fab_mc import meso_mc as mc_np
@@ -9,7 +10,7 @@ from meso_fab_mc import meso_mc as mc_np
 # gradu[2,2] = -gradu[0,0] - gradu[1,1]
 
 
-gradu = jnp.array([[ 0.5,  0. ,  1. ],\
+gradu = np.array([[ 0.5,  0. ,  1. ],\
                   [ 0.6 ,  0.5,  0. ],\
                     [ 1. ,  0. , -1. ]])
 # gradu[0,2] = 1
@@ -22,29 +23,61 @@ gradu = jnp.array([[ 0.5,  0. ,  1. ],\
 
 
 dt = 0.1
-tmax = 10
+tmax = 5
 T = -5
-x = jnp.array([0,0.2,3])
+x = jnp.array([1,0.1,2])
 #x=None
 ## Discrete
+t,nsteps = mc_jax.time(dt,tmax)
+gradu_tile,dt_tile,x_tile = mc_jax.tile_arrays(gradu,dt,tmax,x)
+#gradu_tile = gradu_tile.at[:,2,2].set(jnp.linspace(0,0,nsteps))
 
-n,m = mc_jax.solve_constant(5000,gradu,dt,tmax,x)
+n,m = mc_jax.solve(5000,gradu_tile,dt_tile,x_tile)
 
 a2 = mc_jax.a2calc(n,m)
-t,nsteps = mc_jax.time(dt,tmax)
+
 
 disc_np = mc_np.solver(5000)
 disc_np.solve_constant(gradu,dt,tmax,x)
+
+disc_np2 = mc_np.solver(5000)
+
+
+a2_np = np.zeros((nsteps,3,3))
+a4_np = np.zeros((nsteps,3,3,3,3))
+
+n = np.zeros((nsteps,5000,3))
+m = np.zeros((nsteps,5000))
+
+a2_np[0,...] = mc_np.a2calc(disc_np2.n)
+a4_np[0,...] = mc_np.a4calc(disc_np2.n)
+
+n[0,...] = disc_np.n
+m[0,...] = disc_np.m
+
+for i in range(nsteps-1):
+
+    disc_np2.iterate(gradu_tile[i,...],dt,x)
+
+    n[i+1,...] = disc_np2.n
+    m[i+1,...] = disc_np2.m 
+    a2_np[i+1,...] = mc_np.a2calc(disc_np2.n,disc_np2.m)
+    a4_np[i+1,...] = mc_np.a4calc(disc_np2.n,disc_np2.m)
+
+
 
 plt.plot(t,a2[:,0,0],'r:',label='JAX')
 plt.plot(t,a2[:,1,1],'g:')
 plt.plot(t,a2[:,2,2],'b:')
 #plt.plot(disc.t,disc.a2[:,0,2],'k:')
 
-plt.plot(disc_np.t,disc_np.a2[:,0,0],'r',label='Numpy')
-plt.plot(disc_np.t,disc_np.a2[:,1,1],'g')
-plt.plot(disc_np.t,disc_np.a2[:,2,2],'b')
+plt.plot(t,a2_np[:,0,0],'r',label='Numpy')
+plt.plot(t,a2_np[:,1,1],'g')
+plt.plot(t,a2_np[:,2,2],'b')
 
+plt.plot(t,disc_np.a2[:,0,0],'r--',label='Numpy Constant')
+plt.plot(t,disc_np.a2[:,1,1],'g--')
+plt.plot(t,disc_np.a2[:,2,2],'b--')
 
 plt.ylim(-0.1,1)
 plt.legend()
