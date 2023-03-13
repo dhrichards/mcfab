a42b5a36f10024f118f5a62452e3625c5c37452e
Daniel Richards <danrichards678@gmail.com>
2023-03-13 11:42:31 +1100

    updated to include jax code

--- meso_fab_mc/__init__.py
+++ meso_fab_mc/__init__.py
@@ -2,11 +2,20 @@ from .meso_mc import solver,a2calc,a4calc,random,isotropic,single_max
 from .static import Static
 
 
+
 try:
     import shtns,scipy
     from .buildharmonics import BuildHarmonics
 except ImportError:
     pass
+
+try:
+    import jax
+    from .meso_mc_jax import *
+    from .static_jax import *
+except ImportError:
+    pass
+
 
 
 
